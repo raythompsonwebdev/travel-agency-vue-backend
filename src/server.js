a10bb6b7735ed9846f43ffbc2fc06f1f29644e95
raythompsonwebdev <ray_thomp@hushmail.com>
2022-04-17 20:55:53 +0100

    update css

--- src/server.js
+++ src/server.js
@@ -43,6 +43,7 @@ const withDB = async (operations, res) => {
 
 app.use("/images", express.static(path.join(__dirname, "./assets")));
 
+//home page
 app.get("/api/home", async (req, res) => {
   await withDB(async db => {
     const homepageitems = await db.collection("homePage").find({}).toArray();
@@ -51,7 +52,7 @@ app.get("/api/home", async (req, res) => {
     res.status(200).json(homepageitems); //use json instead of send
   }, res);
 });
-
+//best Deals
 app.get("/api/bestdeals", async (req, res) => {
   await withDB(async db => {
     const bestdealitems = await db
@@ -62,12 +63,12 @@ app.get("/api/bestdeals", async (req, res) => {
   }, res);
 });
 
-app.get("/api/bestdeals/:bestdealId", async (req, res) => {
-  const { bestdealId } = req.params;
+app.get("/api/bestdeal/:itemid", async (req, res) => {
+  const { itemid } = req.params;
   await withDB(async db => {
     const bestdealitem = await db
       .collection("bestdealitems")
-      .findOne({ id: bestdealId });
+      .findOne({ itemid: itemid });
     if (bestdealitem) {
       res.status(200).json(bestdealitem);
     } else {
@@ -75,7 +76,7 @@ app.get("/api/bestdeals/:bestdealId", async (req, res) => {
     }
   }, res);
 });
-
+//holiday packages
 app.get("/api/holidaypackages", async (req, res) => {
   await withDB(async db => {
     const holidaypackageitems = await db
@@ -86,12 +87,12 @@ app.get("/api/holidaypackages", async (req, res) => {
   }, res);
 });
 
-app.get("/api/holidaypackages/:holidaypackageId", async (req, res) => {
-  const { holidaypackageId } = req.params;
+app.get("/api/holidaypackage/:itemid", async (req, res) => {
+  const { itemid } = req.params;
   await withDB(async db => {
     const holidaypackageitem = await db
       .collection("holidaypackageitems")
-      .findOne({ id: holidaypackageId });
+      .findOne({ itemid: itemid });
     if (holidaypackageitem) {
       res.status(200).json(holidaypackageitem);
     } else {
@@ -99,6 +100,103 @@ app.get("/api/holidaypackages/:holidaypackageId", async (req, res) => {
     }
   }, res);
 });
+
+// //get users cart
+// app.get('/api/users/:userId/cart', async (req, res) => {
+//   const { userId } = req.params;
+//   const client = await MongoClient.connect(
+//     process.env.DB_USER && process.env.DB_PASS ?
+//       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aqewv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`:`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       },
+//   );
+//   const db = client.db(process.env.MONGO_DBNAME || 'travelagency');  
+//   const user = await db.collection('users').findOne({ id: userId });
+//   if (!user) return res.status(404).json('Could not find user!');
+//   const products = await db.collection('cart').find({}).toArray();
+//   const cartItemIds = user.cartItems;
+//   const cartItems = cartItemIds.map(id =>
+//     products.find(product => product.id === id));
+//   res.status(200).json(cartItems);
+//   client.close();
+// });
+// // add to users cart
+// app.post('/api/users/:userId/cart', async (req, res) => {
+//   const { userId } = req.params;
+//   //get id of product to add to cart
+//   const { productId } = req.body;
+//   const client = await MongoClient.connect(
+//     process.env.DB_USER && process.env.DB_PASS ?
+//       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aqewv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`:`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       },
+//   );
+//     const db = client.db(process.env.MONGO_DBNAME || 'travelagency');
+//   await db.collection('users').updateOne({ id: userId }, {
+//     $addToSet: { cartItems: productId },
+//   });
+//   const user = await db.collection('users').findOne({ id: userId });
+//   const products = await db.collection('cart').find({}).toArray();
+//   const cartItemIds = user.cartItems;
+//   const cartItems = cartItemIds.map(id =>
+//     products.find(product => product.id === id));
+//   res.status(200).json(cartItems);
+//   client.close();
+// });
+// //delet item from cart
+// app.delete('/api/users/:userId/cart/:holidaypackageId', async (req, res) => {
+//   const { userId, productId } = req.params;
+//   const client = await MongoClient.connect(
+//     process.env.DB_USER && process.env.DB_PASS ?
+//       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aqewv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`:`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       },
+//   );
+//     const db = client.db(process.env.MONGO_DBNAME || 'travelagency');
+
+//   await db.collection('users').updateOne({ id: userId }, {
+//     $pull: { cartItems: productId },
+//   });
+//   const user = await db.collection('users').findOne({ id: userId });
+//   const products = await db.collection('cart').find({}).toArray();
+//   const cartItemIds = user.cartItems;
+//   const cartItems = cartItemIds.map(id =>
+//     products.find(product => product.id === id));
+
+//   res.status(200).json(cartItems);
+//   client.close();
+// });
+
+// app.delete('/api/users/:userId/cart/:bestdealId', async (req, res) => {
+//   const { userId, productId } = req.params;
+//   const client = await MongoClient.connect(
+//     process.env.DB_USER && process.env.DB_PASS ?
+//       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aqewv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`:`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       },
+//   );
+//     const db = client.db(process.env.MONGO_DBNAME || 'travelagency');
+
+//   await db.collection('users').updateOne({ id: userId }, {
+//     $pull: { cartItems: productId },
+//   });
+//   const user = await db.collection('users').findOne({ id: userId });
+//   const products = await db.collection('cart').find({}).toArray();
+//   const cartItemIds = user.cartItems;
+//   const cartItems = cartItemIds.map(id =>
+//     products.find(product => product.id === id));
+
+//   res.status(200).json(cartItems);
+//   client.close();
+// });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
